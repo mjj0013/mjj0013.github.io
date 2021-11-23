8e1d9a02fae0aaf2b2909502a2894f4ef85a3c11
mjj0013 <59294695+mjj0013@users.noreply.github.com>
2021-11-23 12:28:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function update() {
         var triangles = xSortedCoverTriangles[phase]
         triangles.value = Math.max(waveform1(this.step*phase), 25);
         
-        //let saturationVal = Math.max(this.waveform2(this.step*phase), 25);
+        //let saturationVal = Math.max(this.waveform2(step*phase), 25);
         for(let a=0; a < triangles.indices.length; ++a) {
             var Tri = coverTriangles[triangles.indices[a]];
             context.beginPath();

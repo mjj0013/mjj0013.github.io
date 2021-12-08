1712314d6278320b257956d57e05b766a608edb3
Matthew Jett <matthew.jett@willbrook.com>
2021-12-08 11:15:46 -0700

    new

--- imageGallery/imageGallery.js
+++ imageGallery/imageGallery.js
@@ -2,11 +2,16 @@ var myHeaders = new Headers();
 myHeaders.set("Access-Control-Allow-Origin", "*");
 myHeaders.set("Access-Control-Request-Headers", "*");
 
-//var filterArray = {}
+
+
+
+
+//REMEMBER TO PUT URLS OF THE SOURCE OF EACH IMAGE, 
+
+
+
 var imageIDs = {}
-
 function createFilter(imagePath, imageId) {
-
     document.getElementById("resultImage").insertAdjacentHTML('beforeend',
     `<filter id="gradient1_filter" color-interpolation-filters="sRGB">
         <feImage href="${imagePath}" result="Result"/>
@@ -14,29 +19,7 @@ function createFilter(imagePath, imageId) {
     </filter>`);
 }
 
-
-function image1Changed() {
-   
-    var newImgPath = document.getElementById("imageSelector1");
-    var img = document.getElementById("image1");
-    // img.style.top="0"
-    // img.width.baseVal.value = 500;
-    // img.height.baseVal.value = 800;
-    
-    img.href.baseVal = `./images/${newImgPath.value}`
-    var imageID = newImgPath.value.split(".")[0];
-    if(imageIDs[imageID]==undefined) {
-        imageIDs[imageID] = {"path":newImgPath.value, "filter":false};
-
-    }
-    var resultImg = document.getElementById("resultImage");
-    resultImg.href.baseVal = `./images/${newImgPath.value}`
-   
-    //document.getElementById("imageManipSVG").appendChild(img);
-}
-
 function image2Changed() {
-    
     var newImgPath = document.getElementById("imageSelector2")
     var img = document.getElementById("image2");
     var imageID = newImgPath.value.split(".")[0];
@@ -51,16 +34,6 @@ function image2Changed() {
     if(imageIDs[imageID]["filter"]==false) {
         createFilter(img.href.baseVal, imageID);
     }
-    
-    //<object data="./images/gradient1.svg" type="image/svg+xml"></object>
-    
-    
-   
-
-
     document.getElementById("resultImage").setAttributeNS("http://www.w3.org/2000/svg","filter",`url(#gradient1_filter)`);
-
-
-    
    
 }

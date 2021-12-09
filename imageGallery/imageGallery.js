3fded916ec4886b242431fe998cfac7e12347a74
Matthew Jett <matthew.jett@willbrook.com>
2021-12-09 15:58:47 -0700

    new

--- imageGallery/imageGallery.js
+++ imageGallery/imageGallery.js
@@ -9,14 +9,20 @@ myHeaders.set("Access-Control-Request-Headers", "*");
 //REMEMBER TO PUT URLS OF THE SOURCE OF EACH IMAGE, 
 
 
+function insertImageFooters() {
+    var galleryImg1=documenet.getElementById("galleryImg1");
+    var galleryImg2=documenet.getElementById("galleryImg2");
+    var galleryImg3=documenet.getElementById("galleryImg3");
+    var galleryImg4=documenet.getElementById("galleryImg4");
+    var galleryImg5=documenet.getElementById("galleryImg5");
+    var galleryImg6=documenet.getElementById("galleryImg6")
 
-var imageIDs = {}
-function createFilter(imagePath, imageId) {
-    document.getElementById("resultImage").insertAdjacentHTML('beforeend',
-    `<filter id="gradient1_filter" color-interpolation-filters="sRGB">
-        <feImage href="${imagePath}" result="Result"/>
-        <feDisplacementMap in="SourceGraphic" in2="Result" scale="200" xChannelSelector="R" yChannelSelector="B"/>
-    </filter>`);
+
+
+    //galleryImg1.insertAdjacent
+
+    var footer1 = `<path fill="grey" id="footer1" d="M0,${galleryImg1.height} C178,159 440,185 ${galleryImg1.width},${galleryImg1.height}" />`
+    galleryImg1.insertAdjacentHTML('beforeend',footer1);
 }
 
 
